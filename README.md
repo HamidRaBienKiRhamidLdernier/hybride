ed268b0faacb1f09f2f0412f13c756f98c84a107
MajorMaad <quentin.audinot@gmail.com>
2015-12-08 16:06:50 -0500

    Download image + affichage

--- README.md
+++ README.md
@@ -16,3 +16,9 @@
 ## Avancement de l'app :
 - Temps d'installation : a voir
 - 22/11 => Interface graphique : titre + boutons : 15 minutes
+- 08/12 => Fonction de téléchargement d'une image sur un serveur distant.
+	- Temps de développement : 40 minutes
+	- Résultats :
+		- Temps de téléchargement moyen : 7123.6 ms
+		- Temps le plus faible : 4636 ms
+		- Temps le plus élevé : 14983 ms
---
+++ hybrid-app/platforms/android/assets/www/js/download.js
@@ -0,0 +1,37 @@
+var timeStart = 0;
+var timeStop = 0;
+var duration = 0;
+
+var url = "http://www.zastavki.com/pictures/originals/2013/Photoshop_Image_of_the_horse_053857_.jpg";
+
+function onBodyLoad () {
+    timeStart = new Date().getTime();
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function downloadFile(){
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+      var imagePath = "/sdcard/cheval.jpg"; // full file path
+      var fileTransfer = new FileTransfer();
+      fileTransfer.download(url, imagePath, function (entry) {
+                console.log(entry.fullPath); // entry is fileEntry object
+                showLink(url);
+      }, function (error) {
+               console.log(error.code);
+      });
+   })
+}
+
+function showLink (url) {
+    var imgEl = document.getElementById("img-dl");
+    imgEl.setAttribute("src", url);
+
+    timeStop = new Date().getTime();
+    duration = timeStop - timeStart;
+    var pEl = document.getElementById("time-dl");
+    pEl.innerHTML = "Download time : " + duration;
+}
+
+function onDeviceReady() {
+    downloadFile();
+}
